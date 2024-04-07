bd32a024321e786a84f2e79e436aff2df329679f
Marcin Kurczewski <dash@wind.garden>
2024-04-07 23:47:08 +0200

    fix keyboard

--- stormtracker/static/js/interaction/keyboard.js
+++ stormtracker/static/js/interaction/keyboard.js
@@ -7,58 +7,41 @@ export class KeyboardInteraction {
   }
 
   handleKeyDown(event) {
-    if (event.shiftKey || event.ctrlKey || event.metaKey || event.altKey) {
-      return;
-    }
+    const key = event.key.toLowerCase();
 
-    if (event.key === "s" || event.key === "S") {
-      this.control.cycleMapStyle();
-    }
+    const keyMap = {
+      s: () => this.control.cycleMapStyle(),
+      g: () => {
+        // toggle dark navigation / satellite
+        if (config.mapStyle === 0) {
+          this.control.switchMapStyle(2);
+        } else {
+          this.control.switchMapStyle(0);
+        }
+      },
+      t: () => this.control.toggleTrack(),
+      f: () => this.control.toggleFollow(),
+      a: () => this.control.toggleAudio(),
+      n: () => this.control.toggleNorthUp(),
+      r: () => this.control.toggleRain(),
+      l: () => this.control.toggleStrikes(),
+      c: () => {
+        if (event.shiftKey) {
+          this.control.toggleRangePolygons();
+        } else {
+          this.control.toggleRangeCircles();
+        }
+      },
+      1: () => this.control.map.zoomTo(8),
+      2: () => this.control.map.zoomTo(10),
+      3: () => this.control.map.zoomTo(11.5),
+      4: () => this.control.map.zoomTo(13),
+      5: () => this.control.map.zoomTo(15),
+    };
 
-    if (event.key === "g" || event.key === "G") {
-      // toggle dark navigation / satellite
-      if (config.mapStyle === 0) {
-        this.control.switchMapStyle(2);
-      } else {
-        this.control.switchMapStyle(0);
-      }
-    }
-
-    if (event.key === "t" || event.key === "T") {
-      this.control.toggleTrack();
-    }
-    if (event.key === "f" || event.key === "F") {
-      this.control.toggleFollow();
-    }
-    if (event.key === "a" || event.key === "A") {
-      this.control.toggleAudio();
-    }
-    if (event.key === "n" || event.key === "N") {
-      this.control.toggleNorthUp();
-    }
-    if (event.key === "r" || event.key === "R") {
-      this.control.toggleRain();
-    }
-    if (event.key === "c" || event.key === "C") {
-      this.control.toggleRangeCircles();
-    }
-    if (event.key === "r" || event.key === "R") {
-      this.control.toggleRangePolygons();
-    }
-    if (event.key === "1") {
-      this.control.map.zoomTo(8);
-    }
-    if (event.key === "2") {
-      this.control.map.zoomTo(10);
-    }
-    if (event.key === "3") {
-      this.control.map.zoomTo(11.5);
-    }
-    if (event.key === "4") {
-      this.control.map.zoomTo(13);
-    }
-    if (event.key === "5") {
-      this.control.map.zoomTo(15);
+    const func = keyMap[key];
+    if (func) {
+      func();
     }
   }
 }
