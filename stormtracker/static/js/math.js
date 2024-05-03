552240d60f09f26be4e3fb16338b52976887a31a
Marcin Kurczewski <dash@wind.garden>
2024-05-03 11:46:44 +0200

    add coordinate math utilities

---
+++ stormtracker/static/js/math.js
@@ -0,0 +1,37 @@
+export const toRadians = (degrees) => {
+  return (degrees * Math.PI) / 180;
+};
+
+export const toDegrees = (radians) => {
+  return (radians * 180) / Math.PI;
+};
+
+export const getBearing = (srcPoint, dstPoint) => {
+  const startLat = toRadians(srcPoint.lat);
+  const startLon = toRadians(srcPoint.lon);
+  const destLat = toRadians(dstPoint.lat);
+  const destLon = toRadians(dstPoint.lon);
+  const dLon = destLon - startLon;
+
+  const y = Math.sin(dLon) * Math.cos(destLat);
+  const x =
+    Math.cos(startLat) * Math.sin(destLat) -
+    Math.sin(startLat) * Math.cos(destLat) * Math.cos(dLon);
+  const brng = Math.atan2(y, x);
+  return (toDegrees(brng) + 360) % 360;
+};
+
+export const getDistance = (srcPoint, dstPoint) => {
+  const R = 6371;
+  const dLat = toRadians(dstPoint.lat - srcPoint.lat);
+  const dLon = toRadians(dstPoint.lon - srcPoint.lon);
+  const lat1 = toRadians(srcPoint.lat);
+  const lat2 = toRadians(dstPoint.lat);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c * 1000;
+  return d;
+};
