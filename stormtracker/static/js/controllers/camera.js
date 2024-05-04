3e3ef8e24a9eb1b39783002a787cd776a60d6c71
Marcin Kurczewski <dash@wind.garden>
2024-05-04 22:19:21 +0200

    reduce cpu load

--- stormtracker/static/js/controllers/camera.js
+++ stormtracker/static/js/controllers/camera.js
@@ -8,12 +8,14 @@ export class CameraController extends EventTarget {
     this.map = map;
     this.geolocation = geolocation;
     this.lastCameraFollowState = null;
-    this.lastNorthUpEnabled = null;
-    this.lastPitchEnabled = null;
-    this.targetZoom = null;
     this.updateCameraInterval = null;
 
-    config.addEventListener("save", () => this.handleConfigChange());
+    this.targetZoom = null;
+    this.targetPitch = null;
+    this.targetPadding = null;
+    this.targetBearing = null;
+
+    config.addEventListener("save", (event) => this.handleConfigChange(event));
     map.on("movestart", (event) => this.handleMapZoom(event));
     map.on("move", (event) => this.handleMapMove(event));
     map.on("zoomstart", (event) => this.handleMapZoom(event));
@@ -37,8 +39,7 @@ export class CameraController extends EventTarget {
   }
 
   handleConfigChange() {
-    if (this.lastCameraFollowState !== config.cameraFollowState) {
-      this.lastCameraFollowState = config.cameraFollowState;
+    if (config.hasChanged(event, (cfg) => cfg.cameraFollowState)) {
       if (config.cameraFollowState === CameraFollowState.Enabled) {
         this.startTracking();
       } else if (config.cameraFollowState === CameraFollowState.Paused) {
@@ -48,12 +49,28 @@ export class CameraController extends EventTarget {
       }
     }
 
-    if (
-      this.lastPitchEnabled !== config.pitchEnabled ||
-      this.lastNorthUpEnabled !== config.northUpEnabled
-    ) {
-      this.lastNorthUpEnabled = config.northUpEnabled;
-      this.lastPitchEnabled = config.pitchEnabled;
+    let reset = false;
+    if (config.hasChanged(event, (cfg) => cfg.pitchEnabled)) {
+      reset = true;
+      if (config.pitchEnabled) {
+        this.targetPitch = 45;
+        this.targetPadding = { top: document.body.scrollHeight / 3 };
+      } else {
+        this.targetPitch = 0;
+        this.targetPadding = { top: 0 };
+      }
+    }
+
+    if (config.hasChanged(event, (cfg) => cfg.northUpEnabled)) {
+      reset = true;
+      if (config.northUpEnabled) {
+        this.targetBearing = 0;
+      } else if (this.geolocation.lastKnownPosition?.bearing) {
+        this.targetBearing = this.geolocation.lastKnownPosition?.bearing;
+      }
+    }
+
+    if (reset) {
       this.updateCamera(true);
       this.restartInterval();
     }
@@ -139,22 +156,17 @@ export class CameraController extends EventTarget {
   updateCamera(reset) {
     const position = this.geolocation.lastKnownPosition;
 
+    let ease = false;
     const params = {};
     if (config.cameraFollowState === CameraFollowState.Enabled && position) {
       params.center = [position.lon, position.lat];
     }
 
-    if (config.pitchEnabled) {
-      params.pitch = 45;
-      params.padding = { top: document.body.scrollHeight / 3 };
-    } else {
-      params.pitch = 0;
-      params.padding = { top: 0 };
-    }
-
-    if (config.northUpEnabled) {
-      params.bearing = 0;
+    if (this.targetBearing !== null) {
+      params.bearing = this.targetBearing;
+      this.targetBearing = null;
     } else if (
+      !config.northUpEnabled &&
       position &&
       position.bearing !== null &&
       (reset || position.speed >= 0.5)
@@ -162,13 +174,30 @@ export class CameraController extends EventTarget {
       params.bearing = position.bearing;
     }
 
+    if (this.targetPitch !== null) {
+      params.pitch = this.targetPitch;
+      this.targetPitch = null;
+      ease = true;
+    }
+
+    if (this.targetPadding !== null) {
+      params.padding = this.targetPadding;
+      this.targetPadding = null;
+      ease = true;
+    }
+
     if (this.targetZoom !== null) {
       params.zoom = this.targetZoom;
       this.targetZoom = null;
+      ease = true;
     }
 
     if (Object.keys(params).length > 0) {
-      this.map.easeTo(params, { isCustom: true });
+      if (ease) {
+        this.map.easeTo(params, { isCustom: true });
+      } else {
+        this.map.flyTo({ ...params, duration: 0 }, { isCustom: true });
+      }
     }
   }
 }
